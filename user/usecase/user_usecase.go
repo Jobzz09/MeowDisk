5886a47f00f9883c114703f3e0bf661b4246da21
Nikita Dolgavin <ndolgavin@wv00754.local>
2022-05-29 13:08:02 +0300

    uploade code

--- user/usecase/user_usecase.go
+++ user/usecase/user_usecase.go
@@ -1,1 +1,42 @@
 package usecase
+
+import (
+	"database/sql"
+	"github.com/Jobzz09/MeowDisk/models"
+	"github.com/Jobzz09/MeowDisk/user/repository"
+	"github.com/go-redis/redis"
+	"net/http"
+)
+
+type UserUseCase struct {
+	UserRepository repository.UserRepository
+}
+
+func NewUserUseCase(db *sql.DB, redis *redis.Client) UserUseCase {
+	userRepository := repository.NewUserRepository(db, redis)
+	return UserUseCase{UserRepository: userRepository}
+}
+
+func (userUcase UserUseCase) SetCoockieinredis(coockie http.Cookie, user models.UserData) error {
+	return userUcase.UserRepository.SetCoockieinredis(coockie, user)
+}
+
+func (userUcase UserUseCase) GetCoockieinredis(user models.UserData) string {
+	return userUcase.UserRepository.GetCoockieinredis(user)
+}
+
+func (userUcase UserUseCase) Deletecoockieinredis(user models.UserData) error {
+	return userUcase.UserRepository.Deletecoockieinredis(user)
+}
+
+func (userUcase UserUseCase) Login(users models.UserData) error {
+	return userUcase.UserRepository.Login(users)
+}
+
+func (userUcase UserUseCase) Logout(users models.UserData) error {
+	return userUcase.UserRepository.Logout(users)
+}
+
+func (userUcase UserUseCase) Register(users models.UserData) error {
+	return userUcase.UserRepository.Register(users)
+}
